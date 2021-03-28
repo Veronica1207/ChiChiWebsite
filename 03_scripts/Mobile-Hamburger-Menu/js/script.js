cb5f8974fe77db26882f0aa7f400d1b72e87d709
Veronica1207 <logvinenkovd@gmail.com>
2021-03-27 21:44:24 -0700

    2021-03-27

---
+++ 03_scripts/Mobile-Hamburger-Menu/js/script.js
@@ -0,0 +1,9 @@
+/* Hamburger Menu */
+
+$(".menu").click(function() {
+	$(".menu").toggleClass("active");
+	$(".navbar-menu").toggleClass("active");
+	$(".navbar-menu").toggle('show');
+});
+
+/* End */
